a1763dd58b643060447df4e9e06c14c00fafd151
Jholman8 <Jacob.holman@comcast.net>
2014-06-06 13:05:09 -0600

    Update ViewController.swift

--- swiftAnimation/ViewController.swift
+++ swiftAnimation/ViewController.swift
@@ -15,6 +15,32 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+@IBAction func buttonTappedUp(AnyObject) {
+        UIView.animateWithDuration(2, animations: {() -> Void in
+            self.boxView.frame.offset(dx: 0, dy: -100)
+            self.boxView.backgroundColor = UIColor.blackColor()
+            })
+    }
+    @IBAction func buttonTappedDown(AnyObject) {
+        UIView.animateWithDuration(3, animations: {() -> Void in
+            self.boxView.frame.offset(dx: 0, dy: +100)
+            self.boxView.backgroundColor = UIColor.blueColor()
+            })
+    }
+    @IBAction func buttonTappedLeft(AnyObject) {
+        UIView.animateWithDuration(3, animations: {() -> Void in
+            self.boxView.frame.offset(dx: -100, dy: 0)
+            self.boxView.backgroundColor = UIColor.redColor()
+            })
+    }
+    @IBAction func buttonTappedRight(AnyObject) {
+     
+        UIView.animateWithDuration(3, animations: {() -> Void in
+            self.boxView.frame.offset(dx: +100, dy: 0)
+            self.boxView.backgroundColor = UIColor.cyanColor()
+            })
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
